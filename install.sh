289e766cf9a84452ec0d3c0545f8606ea5e20442
PHCitizen <75726261+PHCitizen@users.noreply.github.com>
2021-02-25 02:53:06 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+cd /tmp
+apt update && apt upgrade -y -f
+apt install jq -y
+
+wget -q https://raw.githubusercontent.com/PHCitizen/v2ray-uuid-manager/main/phcv2raymanager
+mv phcv2raymanager /usr/local/sbin/phcv2raymanager
+chmod +x /usr/local/sbin/phcv2raymanager
+clear
+echo "========================================"
+echo "use phcv2raymanager to manage your UUID"
+echo "========================================"
+
